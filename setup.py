4ad95571357c3b79f2332017cea4cb53f1724cb8
Felix Simkovic <felixsimkovic@me.com>
2016-11-10 13:36:15 +0000

    bug fix

--- setup.py
+++ setup.py
@@ -15,27 +15,14 @@ setup(
     author_email='felixsimkovic@me.com',
     license='GNU General Public License',
     url='https://github.com/fsimkovic/conkit',
-    download_url='https://github.com/fsimkovic/conkit/tarball/0.1.1',
-    package_dir={
-        'conkit': 'conkit'
-    },
+    download_url='https://github.com/fsimkovic/conkit/tarball/' + __version__,
+    package_dir={'conkit': 'conkit'},
     packages=find_packages(exclude="tests"),
     scripts=[script for script in glob.glob("scripts/*")],
-    platforms=[
-        'Linux',
-        'Mac OS-X',
-        'Unix',
-    ],
-    install_requires=[
-        'biopython >=1.64',
-        'matplotlib >=1.3.1',
-        'numpy >=1.8.2',
-        'scipy >=0.16.0',
-    ],
+    platforms=['Linux', 'Mac OS-X', 'Unix'],
+    install_requires=['biopython >=1.64', 'matplotlib >=1.3.1', 'numpy >=1.8.2', 'scipy >=0.16.0'],
     test_suite='nose.collector',
-    tests_require=[
-        'nose >=1.3.7',
-    ],
+    tests_require=['nose >=1.3.7'],
     include_package_data=True,
     zip_safe=False,
 )
