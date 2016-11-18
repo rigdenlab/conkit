b221b6a70e8997268e673baab76803dbc024f79c
Felix Simkovic <felixsimkovic@me.com>
2016-11-18 15:29:03 +0000

    version update

--- conkit/_version.py
+++ conkit/_version.py
@@ -2,7 +2,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 1, 1)
+__version_info__ = (0, 1, 2)
 
 # ======================================================
 # Do __NOT__ change anything below here
