85e1ea3071df8c64cd076b0603594268a0bf6b0e
Felix Simkovic <felixsimkovic@me.com>
2016-12-12 14:04:35 +0000

    version v0.3.0

--- conkit/_version.py
+++ conkit/_version.py
@@ -2,7 +2,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 2, 0)
+__version_info__ = (0, 3, 0)
 
 # ======================================================
 # Do __NOT__ change anything below here
