72ed03461fc7f469b0b8550df2d9cd9fc304e00b
Felix Simkovic <felixsimkovic@me.com>
2017-01-24 16:45:01 +0000

    layout improved, logo now better positioned and GitHub tag aligned with remaining text; code for layout moved to layout html

--- docs/conf.py
+++ docs/conf.py
@@ -191,7 +191,7 @@ html_favicon = 'favicon.ico'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path
-#html_style = 'custom.css'
+# html_style = 'custom.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -205,8 +205,7 @@ html_static_path = ['_static']
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
-# The empty string is equivalent to '%b %d, %Y'.
-html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%d %b %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -235,7 +234,7 @@ html_last_updated_fmt = '%b %d, %Y'
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = False
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
