a659262f68e7325122b8b469f46858b084b7eb09
Felix Simkovic <felixsimkovic@me.com>
2017-02-23 12:34:36 +0000

    version bump

--- conkit/_version.py
+++ conkit/_version.py
@@ -2,7 +2,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 6)
+__version_info__ = (0, 6, 1)
 
 # ======================================================
 # Do __NOT__ change anything below here
