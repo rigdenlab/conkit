c547ca3d64881567b8163af3ed5e771f4fbbbea1
Felix Simkovic <felixsimkovic@me.com>
2017-05-16 12:09:15 +0100

    conkit verison modified to dev

--- conkit/_version.py
+++ conkit/_version.py
@@ -2,7 +2,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 7)
+__version_info__ = (0, 7, '1.dev')
 
 # ======================================================
 # Do __NOT__ change anything below here
