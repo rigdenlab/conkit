27a315541e6dba61fe4335fbf410f5ef575e0aa2
Felix Simkovic <felixsimkovic@me.com>
2017-06-02 09:16:33 +0100

    fix bug for ignoring custom.css for #26

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.abspath('.'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.5.1'
+needs_sphinx = '1.6.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -198,12 +198,16 @@ html_favicon = '_static/favicon.ico'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path
-html_style = 'custom.css'
+
+def setup(app):
+    app.add_stylesheet("custom.css")
+
+#html_style = 'custom.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
