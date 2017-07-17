96a04e8ebf619a2fb1d98ec0f4fbd148305a2f2f
Felix Simkovic <felixsimkovic@me.com>
2017-07-17 07:57:36 +0100

    v0.8.2 with essential fixes

--- conkit/version.py
+++ conkit/version.py
@@ -32,7 +32,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 8, 1)
+__version_info__ = (0, 8, 2)
 
 # ======================================================
 # Do __NOT__ change anything below here
