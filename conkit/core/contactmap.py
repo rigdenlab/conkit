1b85f6347dcf294f6a567df90f773e7d7b1176a4
Felix Simkovic <felixsimkovic@me.com>
2017-12-04 13:41:22 +0000

    typo fixed

--- conkit/core/contactmap.py
+++ conkit/core/contactmap.py
@@ -236,8 +236,8 @@ class ContactMap(_Entity):
             warnings.warn("No matches or mismatches found in your contact map. " "Match two ContactMaps first.")
             return 0.0
         elif uk_count > 0:
-            warnings.wargn("Some contacts between the ContactMaps are unmatched due to non-identical "
-                           "sequences. The precision value might be inaccurate.")
+            warnings.warn("Some contacts between the ContactMaps are unmatched due to non-identical "
+                          "sequences. The precision value might be inaccurate.")
 
         return tp_count / (tp_count + fp_count)
 
