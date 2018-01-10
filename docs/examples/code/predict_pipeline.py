b3401848da263d2ae77c9c66d2f21c5218308154
Felix Simkovic <felixsimkovic@me.com>
2018-01-10 10:40:30 +0000

    updated docs to reflect new plot API

--- docs/examples/code/predict_pipeline.py
+++ docs/examples/code/predict_pipeline.py
@@ -19,16 +19,15 @@ sequence_file = "toxd/toxd.fasta"
 sequence_format = "fasta"
 
 # Define the paths to the software we use
-hhblits_exe = "path/to/hhblits"                 # <-- MODIFY THIS
-hhblits_database = "path/to/hhblits_database"   # <-- MODIFY THIS
-ccmpred_exe = "path/to/ccmpred"                 # <-- MODIFY THIS
+hhblits_exe = "path/to/hhblits"  # <-- MODIFY THIS
+hhblits_database = "path/to/hhblits_database"  # <-- MODIFY THIS
+ccmpred_exe = "path/to/ccmpred"  # <-- MODIFY THIS
 
 # Generate a Multiple Sequence Alignment
 print("Generating the Multiple Sequence Alignment")
 a3m_file = "toxd/toxd.a3m"
 hhblits_cline = conkit.applications.HHblitsCommandLine(
-    cmd=hhblits_exe, database=hhblits_database, input=sequence_file, oa3m=a3m_file
-)
+    cmd=hhblits_exe, database=hhblits_database, input=sequence_file, oa3m=a3m_file)
 hhblits_cline()  # Execute HHblits
 
 # Analyse the alignment
@@ -38,8 +37,9 @@ print("Total Number of Sequences:        %d" % msa.nseq)
 print("Number of Effective Sequences:    %d" % msa.neff)
 
 # Plot the amino acid coverage per position in the alignment
+fig1 = conkit.plot.SequenceCoverageFigure(msa)
 seq_cov_file = "toxd/toxd.freq.png"
-conkit.plot.SequenceCoverageFigure(msa, file_name=seq_cov_file)
+fig2.savefig(seq_cov_file)
 print("Sequence Coverage Plot:           %s" % seq_cov_file)
 
 # Convert the alignment into a CCMpred-readable format
@@ -49,20 +49,19 @@ conkit.io.write(jones_file, "jones", msa)
 # Predict the contacts
 print("Predicting contacts")
 mat_file = "toxd/toxd.mat"
-ccmpred_cline = conkit.applications.CCMpredCommandLine(
-    alnfile=jones_file, matfile=mat_file
-)
+ccmpred_cline = conkit.applications.CCMpredCommandLine(alnfile=jones_file, matfile=mat_file)
 ccmpred_cline()  # Execute CCMpred
 
 # Plot the top-30 contacts
-conpred = conkit.io.read(mat_file,  "ccmpred").top_map
+conpred = conkit.io.read(mat_file, "ccmpred").top_map
 # Remove contacts of neigbouring residues
 conpred.remove_neighbors(inplace=True)
 # Sort the list of contacts by their score
 conpred.sort("raw_score", reverse=True, inplace=True)
-conpred = conpred[:30]                                  # Slice the contact map
+conpred = conpred[:30]  # Slice the contact map
+fig2 = conkit.plot.ContactMapFigure(conpred, legend=True)
 contact_map_file = "toxd/toxd.map.png"
-conkit.plot.ContactMapFigure(conpred, file_name=contact_map_file)
+fig2.savefig(contact_map_file)
 print("Contact Map Plot:                 %s" % contact_map_file)
 
 # Convert the contact prediction to a standardised format
