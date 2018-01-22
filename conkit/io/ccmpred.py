5727b82e84feed1641ac9f3d725c5afc6129b2fb
Felix Simkovic <felixsimkovic@me.com>
2018-01-22 15:06:40 +0000

    separate data by tabs

--- conkit/io/ccmpred.py
+++ conkit/io/ccmpred.py
@@ -51,9 +51,10 @@ class CCMpredParser(ContactFileParser):
     Class to parse a CCMpred contact matrix
 
     """
+
     def __init__(self):
         super(CCMpredParser, self).__init__()
-    
+
     def read(self, f_handle, f_id="ccmpred"):
         """Read a contact file
 
@@ -82,11 +83,11 @@ class CCMpredParser(ContactFileParser):
                 if res1_seq > res2_seq:
                     continue
                 # Matrix starts count at 0 so increment numbers by one straight away
-                contact = Contact(int(res1_seq+1), int(res2_seq+1), float(raw_score))
+                contact = Contact(int(res1_seq + 1), int(res2_seq + 1), float(raw_score))
                 contact_map.add(contact)
 
         return contact_file
-        
+
     def _get_contact_pairs(self, mat):
         """Get all contact pairs in the matrix
 
@@ -141,7 +142,7 @@ class CCMpredParser(ContactFileParser):
             for contact in contact_map:
                 mat[contact.res1_seq - 1][contact.res2_seq - 1] = contact.raw_score
                 mat[contact.res2_seq - 1][contact.res1_seq - 1] = contact.raw_score
-            
-            np.savetxt(f_handle, mat)
+
+            np.savetxt(f_handle, mat, delimiter="\t")
 
         return
