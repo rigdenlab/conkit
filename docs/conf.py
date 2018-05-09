7852b98843b2e2340d0693cfd70492c3d9422681
Felix Simkovic <felixsimkovic@me.com>
2018-05-09 15:56:40 +0100

    removed some unnecessary stuff

--- docs/conf.py
+++ docs/conf.py
@@ -354,7 +354,6 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
