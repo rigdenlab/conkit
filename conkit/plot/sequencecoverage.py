b6fe03d74c15a618ecbf1005adedec4cb1003796
Felix Simkovic <felixsimkovic@me.com>
2018-06-05 12:46:41 +0100

    fix plot for get_frequency update

--- conkit/plot/sequencecoverage.py
+++ conkit/plot/sequencecoverage.py
@@ -111,7 +111,7 @@ class SequenceCoverageFigure(Figure):
 
     def draw(self):
         residues = np.arange(1, self._hierarchy.top_sequence.seq_len + 1)
-        aa_counts = np.asarray(self._hierarchy.get_frequency("X")) * self._hierarchy.nseq
+        aa_counts = np.asarray(self._hierarchy.get_frequency("X"))
 
         self.ax.plot(
             residues,
