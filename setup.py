717d1e84e41edb4620ec25a73fe6f1df58c4a982
Felix Simkovic <felixsimkovic@me.com>
2018-07-10 22:19:37 +0100

    initial test to support Python 3.7

--- setup.py
+++ setup.py
@@ -161,6 +161,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
 
