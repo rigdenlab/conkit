941eba416f1ec9cf03e77113c7a8aa26ee74411e
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-08-20 15:35:34 +0100

    added map_align to conkit.applications

--- conkit/plot/modelvalidation.py
+++ conkit/plot/modelvalidation.py
@@ -27,7 +27,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""A module to produce a domain boundary plot"""
+"""A module to produce a model validation plot"""
 
 from __future__ import division
 from __future__ import print_function
