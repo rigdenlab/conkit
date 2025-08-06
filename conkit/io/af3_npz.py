cda514f78538eac1d17f68a6323a661cb4e9422b
AderikVoorspoels <aderik.voorspoels@kuleuven.be>
2025-08-06 14:57:56 +0200

    added parsing of af3 distograms, also unbroke rosettanpz io if you want to to proteins (worked fine for rna)

--- conkit/io/af3_npz.py
+++ conkit/io/af3_npz.py
@@ -1,6 +1,6 @@
 # BSD 3-Clause License
 #
-# Copyright (c) 2016-21, University of Liverpool
+# Copyright (c) 2016-25, University of Liverpool
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,7 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-Parser module specific to rosetta NPZ distance predictions
+Parser module specific to Alphafold3 NPZ distogram predictions
 """
 
 import numpy as np
@@ -37,17 +37,23 @@ from conkit.core.distance import Distance
 from conkit.core.distogram import Distogram
 from conkit.core.distancefile import DistanceFile
 
-DISTANCE_BINS = ((0, 2), (2, 2.5), (2.5, 3), (3, 4), (4, 4.5), (4.5, 5), (5, 5.5), (5.5, 6), (6, 6.5), (6.5, 7),
-                 (7, 7.5), (7.5, 8), (8, 8.5), (8.5, 9), (9, 9.5), (9.5, 10), (10, 10.5), (10.5, 11), (11, 11.5),
-                 (11.5, 12), (12, 12.5), (12.5, 13), (13, 13.5), (13.5, 14), (14, 14.5), (14.5, 15), (15, 15.5),
-                 (15.5, 16), (16, 16.5), (16.5, 17), (17, 17.5), (17.5, 18), (18, 18.5), (18.5, 19), (19, 19.5),
-                 (19.5, 20), (20, np.inf))
+# magic that defines af3 distogram bin borders taken from af3 git
+DISTANCE_BINS = ((0, 2.3125),(2.3125,2.625),(2.625,2.9375),(2.9375,3.25),(3.25,3.5625),(3.5625,3.875),
+                 (3.875,4.1875),(4.1875,4.5),(4.5,4.8125),(4.8125,5.125),(5.125,5.4375),(5.4375,5.75),
+                 (5.75,6.0625),(6.0625,6.375),(6.375,6.6875),(6.6875,7.0),(7.0,7.3125),(7.3125,7.625),
+                 (7.625,7.9375),(7.9375,8.25),(8.25,8.5625),(8.5625,8.875),(8.875,9.1875),(9.1875,9.5),
+                 (9.5,9.8125),(9.8125,10.125),(10.125,10.4375),(10.4375,10.75),(10.75,11.0625),(11.0625,11.375),
+                 (11.375,11.6875),(11.6875,12.0),(12.0,12.3125),(12.3125,12.625),(12.625,12.9375),(12.9375,13.25),
+                 (13.25,13.5625),(13.5625,13.875),(13.875,14.1875),(14.1875,14.5),(14.5,14.8125),(14.8125,15.125),
+                 (15.125,15.4375),(15.4375,15.75),(15.75,16.0625),(16.0625,16.375),(16.375,16.6875),(16.6875,17.0),
+                 (17.0,17.3125),(17.3125,17.625),(17.625,17.9375),(17.9375,18.25),(18.25,18.5625),(18.5625,18.875),
+                 (18.875,19.1875),(19.1875,19.5),(19.5,19.8125),(19.8125,20.125),(20.125,20.4375),(20.4375,20.75),
+                 (20.75,21.0625),(21.0625,21.375),(21.375,21.6875), (21.6875, np.inf)) 
 
+class AF3NpzParser(BinaryDistanceFileParser):
+    """Parser class for Alphafol 3 NPZ distogram file"""
 
-class RosettaNpzParser(BinaryDistanceFileParser):
-    """Parser class for rosetta NPZ distance prediction file"""
-
-    def read(self, f_handle, f_id="rosettanpz", atom_type = "CA"):
+    def read(self, f_handle, f_id="af3npz"):
         """Read a distance prediction file
 
         Parameters
@@ -62,27 +68,19 @@ class RosettaNpzParser(BinaryDistanceFileParser):
         :obj:`~conkit.core.distancefile.DistanceFile`
 
         """
-        #atom = "C1'"
 
         hierarchy = DistanceFile(f_id)
-        hierarchy.original_file_format = "rosettanpz"
+        hierarchy.original_file_format = "af3npz"
         _map = Distogram("distogram_1")
         hierarchy.add(_map)
 
         prediction = np.load(f_handle, allow_pickle=True)
-        if 'dist' in prediction.keys():
-            probs = prediction['dist']
-        elif 'distance' in prediction.keys():
-            probs = prediction['distance'].item()[atom_type]
-        else:
-            print('no distograms found in rosettanpz')
-        # Bin #0 corresponds with d>20A & bins #1 ~ #36 correspond with 2A<d<20A in increments of 0.5A
-        probs = probs[:, :, [x for x in range(1, 37)] + [0]]
+        distogram_array = prediction['distogram']
 
-        L = probs.shape[0]
+        L = distogram_array.shape[0]
         for i in range(L):
             for j in range(i, L):
-                _distance = Distance(i + 1, j + 1, tuple(probs[i, j, :].tolist()), DISTANCE_BINS)
+                _distance = Distance(i + 1, j + 1, tuple(distogram_array[i, j, :].tolist()), DISTANCE_BINS)
                 _map.add(_distance)
 
         return hierarchy
